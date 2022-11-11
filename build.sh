3211b4c74601d96017fb7a055aa521ace3a08de9
chenjia404 <chenjiablog@gmail.com>
2022-11-12 01:36:41 +0800

    删除中间文件，添加linux-arm64

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@ xgo -out EthTweet_${tag}  -targets="darwin/amd64,windows-6.0/amd64,linux/amd64,l
 
 upx EthTweet_${tag}-darwin-10.12-amd64
 mv EthTweet_${tag}-darwin-10.12-amd64 EthTweet
-zip EthTweet_${tag}-darwin-10.12-amd64.zip EthTweet
+zip -m EthTweet_${tag}-darwin-10.12-amd64.zip EthTweet
 
 upx EthTweet_${tag}-linux-amd64
 mv EthTweet_${tag}-linux-amd64 EthTweet
-zip EthTweet_${tag}-linux-amd64.zip EthTweet
+zip -m EthTweet_${tag}-linux-amd64.zip EthTweet
+
+upx EthTweet_${tag}-linux-arm64
+mv EthTweet_${tag}-linux-arm64 EthTweet
+zip -m EthTweet_${tag}-linux-arm64.zip EthTweet
 
 upx EthTweet_${tag}-windows-6.0-amd64.exe
 mv EthTweet_${tag}-windows-6.0-amd64.exe EthTweet.exe
-zip EthTweet_${tag}-windows-6.0-amd64.zip EthTweet.exe
+zip -m EthTweet_${tag}-windows-6.0-amd64.zip EthTweet.exe
