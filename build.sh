e46a764e8d46c30401ba0a3826883928fe3690e8
chenjia404 <chenjiablog@gmail.com>
2022-11-12 01:05:39 +0800

    多平台构建脚本

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tag="0.1.9"
+xgo -out EthTweet_${tag}  -targets="darwin/amd64,windows-6.0/amd64,linux/amd64,linux/arm64" -ldflags="-w -s" .
+
+upx EthTweet_${tag}-darwin-10.12-amd64
+mv EthTweet_${tag}-darwin-10.12-amd64 EthTweet
+zip EthTweet_${tag}-darwin-10.12-amd64.zip EthTweet
+
+upx EthTweet_${tag}-linux-amd64
+mv EthTweet_${tag}-linux-amd64 EthTweet
+zip EthTweet_${tag}-linux-amd64.zip EthTweet
+
+upx EthTweet_${tag}-windows-6.0-amd64.exe
+mv EthTweet_${tag}-windows-6.0-amd64.exe EthTweet.exe
+zip EthTweet_${tag}-windows-6.0-amd64.zip EthTweet.exe
