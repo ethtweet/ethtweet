138898da5d355b98c2783c7b879c1bb81a4ffcea
chenjia404 <chenjiablog@gmail.com>
2022-11-18 03:53:52 +0800

    0.2.8:简化输出，增加引导文件

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-tag="0.1.9"
-xgo -out EthTweet_${tag}  -targets="darwin/amd64,windows-6.0/amd64,linux/amd64,linux/arm64" -ldflags="-w -s" .
+tag="0.2.8"
 
-upx EthTweet_${tag}-darwin-10.12-amd64
-mv EthTweet_${tag}-darwin-10.12-amd64 EthTweet
-zip -m EthTweet_${tag}-darwin-10.12-amd64.zip EthTweet
+xgo -out EthTweet_${tag}  -targets="windows-6.0/amd64,linux/amd64" -ldflags="-w -s" .
+
 
 upx EthTweet_${tag}-linux-amd64
 mv EthTweet_${tag}-linux-amd64 EthTweet
 zip -m EthTweet_${tag}-linux-amd64.zip EthTweet
-
-upx EthTweet_${tag}-linux-arm64
-mv EthTweet_${tag}-linux-arm64 EthTweet
-zip -m EthTweet_${tag}-linux-arm64.zip EthTweet
 
 upx EthTweet_${tag}-windows-6.0-amd64.exe
 mv EthTweet_${tag}-windows-6.0-amd64.exe EthTweet.exe
