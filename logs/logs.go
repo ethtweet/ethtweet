f52d14bcbe417ee4cf2452696e94201240c49b42
chenjia404 <chenjiablog@gmail.com>
2022-11-19 18:41:35 +0800

    固定格式

--- logs/logs.go
+++ logs/logs.go
@@ -5,15 +5,16 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethtweet/ethtweet/global"
 	"github.com/gookit/color"
 )
 
 var IsPrintLog = true
 var IsDebugPrint = true
 
-//这个包用来统一的日志输出处理
-//目前只做简单两个方法 后续根据具体需要在这里增加日志操作
+const DateTimeFormatStr = "2006-01-02 15:04:05"
+
+// 这个包用来统一的日志输出处理
+// 目前只做简单两个方法 后续根据具体需要在这里增加日志操作
 func Println(v ...interface{}) {
 	if IsPrintLog {
 		log.Println(v...)
@@ -21,7 +22,7 @@ func Println(v ...interface{}) {
 }
 
 func print2(color2 color.Color, v ...interface{}) {
-	s := time.Now().Format(global.DateTimeFormatStr)
+	s := time.Now().Format(DateTimeFormatStr)
 	v = append([]interface{}{"[" + s + "]"}, v...)
 	color2.Light().Println(v...)
 }
