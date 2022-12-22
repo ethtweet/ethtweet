57d33dcc42a1942229f21a0a20262d8c3f297109
chenjia404 <chenjiablog@gmail.com>
2022-12-23 01:26:56 +0800

    0.4.4:自动自增nonce，debug模式才显示api列表

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-tag="0.2.9"
+tag="0.4.4"
 
-xgo -out EthTweet_ -go go-1.19.x -targets="windows-6.0/amd64,linux/amd64" -ldflags="-w -s" .
+xgo -out EthTweet -go go-1.19.x -targets="windows-6.0/amd64,linux/amd64" -ldflags="-w -s" .
 
 
 upx EthTweet-linux-amd64
 mv EthTweet-linux-amd64 EthTweet
+chmod +x EthTweet
 zip -m EthTweet-${tag}-linux-amd64.zip EthTweet
 
 upx EthTweet-windows-6.0-amd64.exe
 mv EthTweet-windows-6.0-amd64.exe EthTweet.exe
-zip -m EthTweet-${tag}-windows-6.0-amd64.zip EthTweet.exe
+zip -m EthTweet-${tag}-windows-amd64.zip EthTweet.exe
