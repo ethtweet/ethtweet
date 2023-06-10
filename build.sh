6a0d0867fd46b6b06951b44d2755a7f48002b8c6
chenjia404 <chenjiablog@gmail.com>
2023-06-11 01:23:22 +0800

    不压缩，本来打包的时候也会压缩

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ tag="0.6.1"
 xgo -out EthTweet -go go-1.20.x -targets="windows-6.0/amd64,linux/amd64" -ldflags="-w -s" .
 
 
-upx EthTweet-linux-amd64
 mv EthTweet-linux-amd64 EthTweet
 chmod 0777 EthTweet
 zip -m EthTweet-${tag}-linux-amd64.zip EthTweet
 sha512sum EthTweet-${tag}-linux-amd64.zip -t > EthTweet-${tag}-linux-amd64.zip.sha512
 
-upx EthTweet-windows-6.0-amd64.exe
 mv EthTweet-windows-6.0-amd64.exe EthTweet.exe
 zip -m EthTweet-${tag}-windows-amd64.zip EthTweet.exe
 sha512sum EthTweet-${tag}-windows-amd64.zip -t > EthTweet-${tag}-windows-amd64.zip.sha512
