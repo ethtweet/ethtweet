48da570f8887fa53d8ef966bfbf41ded2e850f55
chenjia404 <chenjiablog@gmail.com>
2023-07-03 02:12:07 +0800

    使用go 1.20 的新的常量

--- global/constant.go
+++ global/constant.go
@@ -1,12 +1,14 @@
 package global
 
+import "time"
+
 // 这个包放置公共常量
 const (
 	IsYes = 1
 	IsNo  = 0
 
-	DateTimeFormatStr = "2006-01-02 15:04:05"
-	DateFormatStr     = "2006-01-02"
+	DateTimeFormatStr = time.DateTime
+	DateFormatStr     = time.DateOnly
 
 	TwitterProtocol = "/ethtweet/0.1"
 	Version         = "0.7.2"
